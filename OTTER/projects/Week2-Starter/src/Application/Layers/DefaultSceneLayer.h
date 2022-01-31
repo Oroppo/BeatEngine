66d56086b83327d6a5a92b7c2ff9e7899ff535c0
Kai Joseph <kai.joseph1@ontariotechu.net>
2022-01-31 18:41:05 -0500

    Included all object spawn functions
    Updated DefailtSceneLayer.h + .cpp to include all of our previous object spawning functions

--- OTTER/projects/Week2-Starter/src/Application/Layers/DefaultSceneLayer.h
+++ OTTER/projects/Week2-Starter/src/Application/Layers/DefaultSceneLayer.h
@@ -10,6 +10,8 @@
  * This example layer handles creating a default test scene, which we will use 
  * as an entry point for creating a sample scene
  */
+//using namespace Gameplay;
+//using namespace Gameplay::Physics;
 class DefaultSceneLayer final : public ApplicationLayer {
 public:
 	MAKE_PTRS(DefaultSceneLayer)
@@ -23,15 +25,51 @@ public:
 	//	LoseState = 2,
 	//};
 
-	void SpawnStartPlat(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material,
-		std::string ObjName, glm::vec3 pos,glm::vec3 rot, glm::vec3 scale);
-
-	void SpawnObj(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
-		glm::vec3 rot, glm::vec3 scale, Gameplay::GameObject::Sptr parent = nullptr);
 
 	// Inherited from ApplicationLayer
 
 	virtual void OnAppLoad(const nlohmann::json& config) override;
+
+	// Object spawning functions for our default scene layer
+	void SpawnObj(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnStartPlat(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnGem(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr MaterialOn, Gameplay::Material::Sptr MaterialOff, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnCollectable(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnCD(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnWallJump(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnBuilding(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnBuilding2(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnBuilding3(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnSmallWallJump(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnSuperSmallWallJump(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnStairsRight(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnStairsLeft(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnSpeaker(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnSquarePlat(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnFloatingLights(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnBackGroundCar(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnForeGroundCar(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+	void SpawnBackGroundBuilding(Gameplay::Scene::Sptr scene, Gameplay::MeshResource::Sptr Mesh, Gameplay::Material::Sptr Material, std::string ObjName, glm::vec3 pos,
+		glm::vec3 rot, glm::vec3 scale);
+
 
 protected:
 	void _CreateScene();
